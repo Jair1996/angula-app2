a1fd6b0668c5b9207ae75960dec49d1e6a5ebd0b
jair <jairdeyner730791@gmail.com>
2022-12-22 14:23:14 -0500

    Reactividad agregado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { TituloComponent } from './components/titulo/titulo.component';
+import { FormularioComponent } from './components/formulario/formulario.component';
+import { FormsModule } from '@angular/forms';
+import { ImagenComponent } from './components/imagen/imagen.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    TituloComponent,
+    FormularioComponent,
+    ImagenComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/imagen/imagen.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-imagen',
+  templateUrl: './imagen.component.html',
+  styleUrls: ['./imagen.component.css'],
+})
+export class ImagenComponent {
+  url: string =
+    'https://as01.epimg.net/meristation/imagenes/2020/12/23/betech/1608737571_792192_1608737676_noticia_normal_recorte1.jpg';
+}
